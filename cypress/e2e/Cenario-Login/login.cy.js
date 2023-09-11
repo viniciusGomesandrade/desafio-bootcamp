11a2967e9348d5715217ef7a15a5137a15b8008a
Vinicius Andrade <vinijk91@gmail.com>
2023-09-11 17:42:10 -0300

    subindo os arquivos

---
+++ cypress/e2e/Cenario-Login/login.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+//Esse cenario de teste tem como objetivo testar a função login.
+//Autor: Vinicius gomes andrade
+
+describe('Primeiro acessar o site', () => {
+    beforeEach(() => {
+      cy.visit('https://seubarriga.wcaquino.me/login')
+    })
+    afterEach( () => {
+        cy.screenshot()
+
+    });
+
+    it('login no site seubarriga', () => {
+        cy.get('#email').type('vinijk91@gmail.com')
+        cy.get('#senha').type('xvrDCXmz5MUGqpD')
+        cy.get('.btn').click()
+        cy.get('.alert').should('contain','Bem vindo')
+    })
+  
+  })
+  
